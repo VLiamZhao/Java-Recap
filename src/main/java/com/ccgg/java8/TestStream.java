d49c257b02f8fec735675d7e01bb6f426f9e7376
Can Zhao <canzhao216@gmail.com>
2020-11-25 12:09:53 -0500

    Nov 25-2

---
+++ src/main/java/com/ccgg/java8/TestStream.java
@@ -0,0 +1,62 @@
+package com.ccgg.java8;
+
+import com.ccgg.code.Employee;
+import com.ccgg.code.Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestStream {
+    public int getBestAverageScore(String[][] array){
+        if(array == null || array.length == 0){
+            return -1;
+        }
+        double result = Arrays.stream(array)
+                .map(arr -> {
+                    if(arr == null || arr.length <= 1){
+                        return -1d;
+                    }
+                    return Arrays.stream(arr).skip(1).mapToDouble(Integer::parseInt)
+                            .average().getAsDouble();
+                }).max((a, b) -> (int)(a - b)).get();
+        return (int)result;
+    }
+
+    public static void main(String[] args) {
+        String[][] array = {{"bob", "23", "45", "98"}, {"ben", "90"}, {"ketty", "78"}};
+        System.out.println(new TestStream().getBestAverageScore(array));
+
+        int[] arr = {1, 2, 3, 5, 2, 1};
+        System.out.println(Arrays.stream(arr).filter(num -> num > 1).sum());
+
+        List<Person> persons = Arrays.asList(
+                new Person("Bob"),
+                new Person("Alex"),
+                new Person("Alice")
+        );
+        Employee emp = persons.stream()
+                .filter(p -> p.getName().startsWith("A"))
+                .map(Person::getName)
+                .map(Employee::new)
+                .findAny()
+                .get();
+        System.out.println(emp);
+
+        List<Long> list = new ArrayList<>();
+        for(long i = 0; i < 1000; i++){
+            list.add(i);
+        }
+
+        System.out.println(
+                list.parallelStream().reduce(0l, (a, b) -> {
+                    try{
+                        Thread.sleep(5);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    return a + b;
+                })
+        );
+    }
+}
